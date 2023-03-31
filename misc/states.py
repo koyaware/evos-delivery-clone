8d53e81bb222e3c3bbdc1a9c65322f78cee7ca8e
koyaware <rundelchaos@gmail.com>
2023-03-31 19:12:37 +0500

    added product amount state

--- misc/states.py
+++ misc/states.py
@@ -3,3 +3,7 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class ProductsIdState(StatesGroup):
     product_id = State()
+
+
+class ProductsAmountState(StatesGroup):
+    product_amount = State()
