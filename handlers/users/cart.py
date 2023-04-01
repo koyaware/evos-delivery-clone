66fdf463e6d16d90a6ac2af76a0ac760ad69b36f
koyaware <rundelchaos@gmail.com>
2023-04-01 12:10:26 +0500

    upgraded my cart menu

--- handlers/users/cart.py
+++ handlers/users/cart.py
@@ -1,23 +1,26 @@
 from aiogram import Dispatcher
+from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from commands.admins import Commands
 from models import Cart, CartProducts, Products
 
 
-async def my_cart(message: Message):
+async def my_cart(message: Message, state: FSMContext):
     users: Cart = await Cart.query.where(
         Cart.user_id == message.from_user.id
     ).gino.all()
     if not users:
         await message.answer("Ваша корзина пуста!")
-    product_keyboad = InlineKeyboardMarkup()
     for user in users:
         carts: CartProducts = await CartProducts.query.where(
             CartProducts.cart_id == user.Id
         ).gino.all()
         if not carts:
             await message.answer("Ваша корзина пуста!")
+        product_keyboard = InlineKeyboardMarkup()
+        product_name = []
+        product_price = []
         for cart in carts:
             products: Products = await Products.query.where(
                 Products.Id == cart.products_id
@@ -25,15 +28,20 @@ async def my_cart(message: Message):
             if not products:
                 await message.answer("Ваша корзина пуста!")
             for product in products:
-                product_keyboad.add(
+                product_keyboard.add(
                     InlineKeyboardButton(f'🗙 {product.name}', callback_data=product.Id)
                 )
+                product_name.append(cart.amount)
+                product_name.append(product.name)
+                product_price.append(product.price)
+                price = sum(map(int, product_price))
         await message.bot.send_message(message.from_user.id,
-                                       f"У вас в корзине: \n - "
-                                       f"<b></b>\n\n"
-                                       f"Стоимость: <b></b>\n"
-                                       f"Доставка: <b>10000</b>",
-                                       reply_markup=product_keyboad)
+                                       f"У вас в корзине: \n"
+                                       f"<b>{chr(10).join([str(i) for i in product_name])}</b>\n\n"
+                                       f"Стоимость: {price}\n"
+                                       f"Доставка: 10000\n"
+                                       f"Общая сумма: <b>{price + 10000}</b>",
+                                       reply_markup=product_keyboard)
 
 
 def register_my_cart_handlers(dp: Dispatcher):
