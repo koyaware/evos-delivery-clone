bec50bf290f6507f7dcc35d53c2937eeab84290a
koyaware <rundelchaos@gmail.com>
2023-04-04 11:40:22 +0500

    code restructure

--- handlers/users/carts/cart.py
+++ handlers/users/carts/cart.py
@@ -1,5 +1,4 @@
 from aiogram import Dispatcher
-from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from commands.admins import Commands
@@ -7,18 +6,18 @@ from keyboards.reply import MY_CART_KEYBOARDS
 from models import Cart, CartProducts, Products
 
 
-async def my_cart(message: Message, state: FSMContext):
+async def my_cart(message: Message):
     users: Cart = await Cart.query.where(
         Cart.user_id == message.from_user.id
     ).gino.all()
     if not users:
-        await message.answer("Ваша корзина пуста!")
+        return await message.answer("Ваша корзина пуста!")
     for user in users:
         carts: CartProducts = await CartProducts.query.where(
             CartProducts.cart_id == user.Id
         ).gino.all()
         if not carts:
-            await message.answer("Ваша корзина пуста!")
+            return await message.answer("Ваша корзина пуста!")
         product_keyboard = InlineKeyboardMarkup()
         product_name = []
         product_price = []
@@ -27,15 +26,18 @@ async def my_cart(message: Message, state: FSMContext):
                 Products.Id == cart.products_id
             ).gino.all()
             if not products:
-                await message.answer("Ваша корзина пуста!")
+                return await message.answer("Ваша корзина пуста!")
             for product in products:
                 product_keyboard.add(
                     InlineKeyboardButton(f'🗙 {product.name}', callback_data=product.Id)
                 )
-                product_name.append(cart.amount)
-                product_name.append(product.name)
+                product_name.append(f"Товар: {product.name}.")
+                product_name.append(f"Количество: {cart.amount}.")
                 product_price.append(product.price)
                 price = sum(map(int, product_price))
+        await message.answer("Для удаления товара, вы можете нажать кнопку очистить корзину "
+                             "или удалить выборочно ниже.",
+                             reply_markup=MY_CART_KEYBOARDS)
         await message.bot.send_message(message.from_user.id,
                                        f"У вас в корзине: \n"
                                        f"<b>{chr(10).join([str(i) for i in product_name])}</b>\n\n"
@@ -43,9 +45,6 @@ async def my_cart(message: Message, state: FSMContext):
                                        f"Доставка: 10000 сум.\n"
                                        f"Общая сумма: <b>{price + 10000} сум.</b>",
                                        reply_markup=product_keyboard)
-        await message.answer("Для удаления товара, вы можете нажать кнопку очистить корзину "
-                             "или удалить выборочно выше.",
-                             reply_markup=MY_CART_KEYBOARDS)
 
 
 def register_my_cart_handlers(dp: Dispatcher):
