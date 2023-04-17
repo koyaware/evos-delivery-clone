d8cf7de7f280e7e4efffe4547ffd51e75d02f797
koyaware <rundelchaos@gmail.com>
2023-04-17 12:00:36 +0500

    added settings / added user authorization

--- handlers/users/user.py
+++ handlers/users/user.py
@@ -5,41 +5,26 @@ from sqlalchemy import and_
 
 from commands.admins import Commands
 from filters.admin import UserFilter
-from keyboards.reply import USER_KEYBOARDS, MENU_KEYBOARDS
-from misc.states import PhoneNumberState
+from keyboards.reply import USER_KEYBOARDS, MENU_KEYBOARDS, SEND_CONTACT_KEYBOARD
 from models import Users
 
 
 async def user_start(message: Message):
     users = await Users.query.where(and_(
         Users.tg_id == message.from_user.id,
-        Users.phone_number == Users.phone_number
+        Users.phone_number == Users.phone_number,
+        Users.location_latitude == Users.location_latitude,
+        Users.location_longitude == Users.location_longitude
     )).gino.all()
     if not users:
-        await PhoneNumberState.phone_number.set()
-        return await message.answer("Введите номер телефона: \nПример: +998901234567")
+        return await message.answer("Отправьте ваш номер телефона.",
+                                    reply_markup=SEND_CONTACT_KEYBOARD)
     is_user = await Users.query.where(
         Users.is_user == True
     ).gino.all()
     if not is_user:
         return await message.answer("У вас нет прав!\nВы забанены!")
     await message.answer("Привет, пользователь! Выбери команду: ", reply_markup=USER_KEYBOARDS)
-
-
-async def user_start_ph_number(message: Message, state: FSMContext):
-    phone_number = await Users.query.where(and_(
-        Users.tg_id == message.from_user.id,
-        Users.phone_number == message.text
-    )).gino.all()
-    if phone_number:
-        return await message.answer('Введено неверное значение!')
-    if message.text.startswith("+998"):
-        if len(message.text) != 13:
-            return await message.answer("Введено неверное значение!\nПопробуйте заново через /start !")
-        await Users.create(tg_id=message.from_user.id, phone_number=message.text)
-        await state.finish()
-        return await message.bot.send_message(message.from_user.id, "Привет, пользователь! Выбери команду: ",
-                                              reply_markup=USER_KEYBOARDS)
 
 
 async def main_menu(message: Message, state: FSMContext):
@@ -49,10 +34,7 @@ async def main_menu(message: Message, state: FSMContext):
 
 def register_user_handlers(dp: Dispatcher):
     dp.register_message_handler(
-        user_start, UserFilter(), text=["!start", "/start", Commands.come_back.value]
-    )
-    dp.register_message_handler(
-        user_start_ph_number, UserFilter(), state=PhoneNumberState.phone_number
+        user_start, text=["!start", "/start", Commands.come_back.value]
     )
     dp.register_message_handler(
         main_menu, UserFilter(), text=Commands.main_menu.value
