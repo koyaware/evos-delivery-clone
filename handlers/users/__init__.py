5229d0b71984102bcd0669c64288657191d9a9d2
koyaware <rundelchaos@gmail.com>
2023-04-17 12:00:26 +0500

    added settings / added user authorization

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -2,10 +2,14 @@ from aiogram import Dispatcher
 
 from .carts import register_all_carts_handlers
 from .menu import register_all_menu_handlers
+from .settings import register_all_settings_handlers
 from .user import register_user_handlers
+from .user_auth import register_all_user_auths
 
 
 def register_all_user_handlers(dp: Dispatcher):
     register_user_handlers(dp)
+    register_all_user_auths(dp)
+    register_all_settings_handlers(dp)
     register_all_menu_handlers(dp)
     register_all_carts_handlers(dp)
